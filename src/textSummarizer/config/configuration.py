f14c2079e15e22bc1ea27acdcbf5304e0e51fac8
tejaswinippatil <tejaswinipawarpatil@gmail.com>
2024-03-14 16:59:49 +0530

    data validation notebook added

--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -2,6 +2,7 @@ from textSummarizer.constants import *
 from textSummarizer.utils.common import read_yaml,create_directories
 
 from textSummarizer.entity import DataIngestionConfig
+from textSummarizer.entity import DatavalidationConfig
 import os 
 
 class ConfigurationManager:
@@ -30,3 +31,15 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    def get_data_validation_config(self) -> DatavalidationConfig:
+       config = self.config.data_validation
+
+       create_directories([config.root_dir])
+
+       data_validation_config = DatavalidationConfig(
+           root_dir= config.root_dir,
+           STATUS_FILE= config.STATUS_FILE,
+           ALL_REQUIRED_FILES= config.ALL_REQUIRED_FILES,
+       )
+       return data_validation_config
